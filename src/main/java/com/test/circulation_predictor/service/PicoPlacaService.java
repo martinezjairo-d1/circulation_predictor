1f2307666f04745bf54404226ae46654e225196e
martinezjairo-d1 <martinezjairo.d1@gmail.com>
2024-09-30 06:26:00 -0500

    Added class that validates inputs.

--- src/main/java/com/test/circulation_predictor/service/PicoPlacaService.java
+++ src/main/java/com/test/circulation_predictor/service/PicoPlacaService.java
@@ -1,6 +1,7 @@
 package com.test.circulation_predictor.service;
 
 import com.test.circulation_predictor.model.PicoPlaca;
+import com.test.circulation_predictor.utilities.InputValidator;
 
 import io.swagger.v3.oas.annotations.Hidden;
 
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 /**
  * Service class that provides logic to determine if a vehicle can circulate
@@ -28,21 +30,36 @@ public class PicoPlacaService {
 	 * @return a String message indicating if the vehicle can circulate or not.
 	 */
 	public String canCirculate(String plateNumber, String date) {
+		// Validating the plate number
+		if (!InputValidator.isValidPlateNumber(plateNumber)) {
+			return "Por favor, ingrese un número de placa válido (ej. ABC-123 o ABC-1234).";
+		}
+
+		// Validating the date
+		if (!InputValidator.isValidDate(date)) {
+			return "Por favor, ingrese una fecha válida (dd/MM/yyyy).";
+		}
+
 		// Extract the last digit from the license plate numbe
 		int lastDigit = Integer.parseInt(plateNumber.substring(plateNumber.length() - 1));
 
 		// Format the provided date String into a LocalDate
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-		LocalDate localDate = LocalDate.parse(date, formatter);
+		LocalDate localDate;
+		try {
+			localDate = LocalDate.parse(date, formatter);
+		} catch (DateTimeParseException e) {
+			return "Error al formatear la fecha. Por favor, use el formato dd/MM/yyyy.";
+		}
 
 		// Create an instance of the PicoPlaca model
 		PicoPlaca picoPlaca = new PicoPlaca();
 
 		// Check if the vehicle can circulate
 		if (picoPlaca.canCirculate(localDate, lastDigit)) {
-			return "Este día puedes circular libremente en todo el distrito metropolitano de Quito.";
+			return "Este día puedes circular libremente en todo el Distrito Metropolitano de Quito.";
 		}
-		return "Este día no puedes circular en la zona del Pico y Placa.:\n\n"
+		return "Este día no puedes circular en la zona de Pico y Placa.:\n\n"
 				+ "Te recordamos que los horarios de restricción son:\n\n" + "- Matutino: 7:00 a.m. - 9:30 a.m.\n"
 				+ "- Vespertino: 4:00 p.m. - 9:00 p.m.";
 	}
